57d33b6d4000888291c568fda6eeaff00139b09c
PhilippKDT <p.kleidt@mbd-team.de>
2024-08-26 11:18:49 +0200

    Zwischenstand

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,56 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+function RespondMouseClickFieldOne() {
+  document.getElementById("click1").innerHTML = "X";
+}
+function RespondMouseClickFieldTwo() {
+  document.getElementById("click2").innerHTML = "X";
+}
+function RespondMouseClickFieldThree() {
+  document.getElementById("click3").innerHTML = "X";
+}
+function RespondMouseClickFieldFour() {
+  document.getElementById("click4").innerHTML = "X";
+}
+function RespondMouseClickFieldFive() {
+  document.getElementById("click5").innerHTML = "X";
+}
+function RespondMouseClickFieldSix() {
+  document.getElementById("click6").innerHTML = "X";
+}
+function RespondMouseClickFieldSeven() {
+  document.getElementById("click7").innerHTML = "X";
+}
+function RespondMouseClickFieldEight() {
+  document.getElementById("click8").innerHTML = "X";
+}
+function RespondMouseClickFieldNine() {
+  document.getElementById("click9").innerHTML = "X";
+}
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const fieldOne = document.getElementById("display");
+fieldOne?.addEventListener("click", RespondMouseClickFieldOne, true);
+
+const fieldTwo = document.getElementById("display");
+fieldTwo?.addEventListener("click", RespondMouseClickFieldTwo, true);
+
+const fieldThree = document.getElementById("display");
+fieldThree?.addEventListener("click", RespondMouseClickFieldThree, true);
+
+const fieldFour = document.getElementById("display");
+fieldFour?.addEventListener("click", RespondMouseClickFieldFour, true);
+
+const fieldFive = document.getElementById("display");
+fieldFive?.addEventListener("click", RespondMouseClickFieldFive, true);
+
+const fieldSix = document.getElementById("display");
+fieldSix?.addEventListener("click", RespondMouseClickFieldSix, true);
+
+const fieldSeven = document.getElementById("display");
+fieldSeven?.addEventListener("click", RespondMouseClickFieldSeven, true);
+
+const fieldEight = document.getElementById("display");
+fieldEight?.addEventListener("click", RespondMouseClickFieldEight, true);
+
+const fieldNine = document.getElementById("display");
+fieldNine?.addEventListener("click", RespondMouseClickFieldNine, true);
