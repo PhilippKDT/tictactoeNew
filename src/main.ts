f6dccf94157a06dc2faf81cd3ace2dad7b5e3541
PhilippKDT <p.kleidt@mbd-team.de>
2024-08-26 11:48:20 +0200

    Ts Errors fixed, Spielerwechselfunktion

--- src/main.ts
+++ src/main.ts
@@ -1,56 +1,68 @@
 import "./style.css";
 
+let playerXorO = 0;
+
+function playerChange() {
+  for (let i = 0; i < 9; i++) {
+    playerXorO += 1;
+    if (playerXorO % 2) {
+      console.log("Player 1");
+    }
+    console.log("Player2");
+  }
+}
+
 function RespondMouseClickFieldOne() {
-  document.getElementById("click1").innerHTML = "X";
+  (<HTMLDivElement>document.getElementById("click1")).innerHTML = "X";
 }
 function RespondMouseClickFieldTwo() {
-  document.getElementById("click2").innerHTML = "X";
+  (<HTMLDivElement>document.getElementById("click2")).innerHTML = "X";
 }
 function RespondMouseClickFieldThree() {
-  document.getElementById("click3").innerHTML = "X";
+  (<HTMLDivElement>document.getElementById("click3")).innerHTML = "X";
 }
 function RespondMouseClickFieldFour() {
-  document.getElementById("click4").innerHTML = "X";
+  (<HTMLDivElement>document.getElementById("click4")).innerHTML = "X";
 }
 function RespondMouseClickFieldFive() {
-  document.getElementById("click5").innerHTML = "X";
+  (<HTMLDivElement>document.getElementById("click5")).innerHTML = "X";
 }
 function RespondMouseClickFieldSix() {
-  document.getElementById("click6").innerHTML = "X";
+  (<HTMLDivElement>document.getElementById("click6")).innerHTML = "X";
 }
 function RespondMouseClickFieldSeven() {
-  document.getElementById("click7").innerHTML = "X";
+  (<HTMLDivElement>document.getElementById("click7")).innerHTML = "X";
 }
 function RespondMouseClickFieldEight() {
-  document.getElementById("click8").innerHTML = "X";
+  (<HTMLDivElement>document.getElementById("click8")).innerHTML = "X";
 }
 function RespondMouseClickFieldNine() {
-  document.getElementById("click9").innerHTML = "X";
+  (<HTMLDivElement>document.getElementById("click9")).innerHTML = "X";
 }
 
-const fieldOne = document.getElementById("display");
-fieldOne?.addEventListener("click", RespondMouseClickFieldOne, true);
+const fieldOne = <HTMLDivElement>document.getElementById("click1");
+fieldOne.addEventListener("click", RespondMouseClickFieldOne, true);
 
-const fieldTwo = document.getElementById("display");
-fieldTwo?.addEventListener("click", RespondMouseClickFieldTwo, true);
+const fieldTwo = <HTMLDivElement>document.getElementById("click2");
+fieldTwo.addEventListener("click", RespondMouseClickFieldTwo, true);
 
-const fieldThree = document.getElementById("display");
-fieldThree?.addEventListener("click", RespondMouseClickFieldThree, true);
+const fieldThree = <HTMLDivElement>document.getElementById("click3");
+fieldThree.addEventListener("click", RespondMouseClickFieldThree, true);
 
-const fieldFour = document.getElementById("display");
-fieldFour?.addEventListener("click", RespondMouseClickFieldFour, true);
+const fieldFour = <HTMLDivElement>document.getElementById("click4");
+fieldFour.addEventListener("click", RespondMouseClickFieldFour, true);
 
-const fieldFive = document.getElementById("display");
-fieldFive?.addEventListener("click", RespondMouseClickFieldFive, true);
+const fieldFive = <HTMLDivElement>document.getElementById("click5");
+fieldFive.addEventListener("click", RespondMouseClickFieldFive, true);
 
-const fieldSix = document.getElementById("display");
-fieldSix?.addEventListener("click", RespondMouseClickFieldSix, true);
+const fieldSix = <HTMLDivElement>document.getElementById("click6");
+fieldSix.addEventListener("click", RespondMouseClickFieldSix, true);
 
-const fieldSeven = document.getElementById("display");
-fieldSeven?.addEventListener("click", RespondMouseClickFieldSeven, true);
+const fieldSeven = <HTMLDivElement>document.getElementById("click7");
+fieldSeven.addEventListener("click", RespondMouseClickFieldSeven, true);
 
-const fieldEight = document.getElementById("display");
-fieldEight?.addEventListener("click", RespondMouseClickFieldEight, true);
+const fieldEight = <HTMLDivElement>document.getElementById("click8");
+fieldEight.addEventListener("click", RespondMouseClickFieldEight, true);
 
-const fieldNine = document.getElementById("display");
-fieldNine?.addEventListener("click", RespondMouseClickFieldNine, true);
+const fieldNine = <HTMLDivElement>document.getElementById("click9");
+fieldNine.addEventListener("click", RespondMouseClickFieldNine, true);
